e4b69334e611abcd5e4b700f7a3c73abe134a1e3
Pelmemant <basil.111@yandex.ru>
2024-11-17 23:20:23 +0700

    Update models.py

--- module_19_1/I/task1/models.py
+++ module_19_1/I/task1/models.py
@@ -11,5 +11,5 @@ class Game(models.Model):
     cost = models.DecimalField(max_digits=8, decimal_places=2)
     size = models.DecimalField(max_digits=50, decimal_places=2)
     description = models.TextField()
-    age_limited = models.BooleanField()
+    age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
